0dacc9ada9106257a1d460e91fb5ced6904f0015
Christian Weber <cmathew.weber@gmail.com>
2022-10-25 09:35:14 -0500

    vim install

---
+++ vim_plugins.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+cd ~/.vim/bundle
+git clone https://github.com/dkarter/bullets.vim.git
+git clone https://github.com/lambdalisue/fern-git-status.vim.git
+git clone https://github.com/lambdalisue/fern-renderer-nerdfont.vim.git
+git clone https://github.com/lambdalisue/fern.vim.git
+git clone https://github.com/lambdalisue/glyph-palette.vim
+git clone https://github.com/junegunn/goyo.vim
+git clone https://github.com/morhetz/gruvbox.git
+git clone https://github.com/lambdalisue/nerdfont.vim.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/vim-airline/vim-airline-themes.git
+git clone https://github.com/panozzaj/vim-autocorrect.git
+git clone https://github.com/ryanoasis/vim-devicons.git
+git clone https://github.com/preservim/vim-lexical.git
+git clone https://github.com/preservim/vim-markdown.git
+git clone https://github.com/sheerun/vim-polyglot.git
+git clone https://github.com/tpope/vim-sensible
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/dhruvasagar/vim-table-mode.git
+git clone https://github.com/preservim/vim-wheel.git
+git clone https://github.com/preservim/vim-wordy.git
+
